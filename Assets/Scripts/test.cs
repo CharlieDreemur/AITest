5d7129375cc28fa37c5d356c5cbb417b862053b6
CharlieDreemur <zhangwj.charlie@gmail.com>
2023-04-05 02:05:35 -0500

    implement odin inspector

--- Assets/Scripts/test.cs
+++ Assets/Scripts/test.cs
@@ -1,18 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.Events;
+using ChatGPTWrapper;
+using Sirenix.OdinInspector;
 public class test : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
+    [SerializeField]
+    private TMP_Text _text;
+    [SerializeField]
+    private TMP_InputField _inputField;
+    [SerializeField]
+    private ChatGPTConversation _chatGPTConversation;
+    public UnityStringEvent chatGPTResponse = new UnityStringEvent();
+    private void Awake() {
+        SetText("Hello World");
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void Update(){
     }
+
+    public void SetText(string str)
+    {
+        _text.text = str;
+    }
+
+    public void SendChatGPTMessage()
+    {
+        chatGPTResponse.Invoke(_inputField.text);
+    }
+
 }
